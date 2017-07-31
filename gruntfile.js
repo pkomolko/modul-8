288469b3ccf9c289e3a48892c4e7b18e0a4dacb6
pkomolkoe <p.komolko@gmail.com>
2017-08-01 00:02:56 +0200

    dodanie pluginu do zmniejszania obrazkow

--- gruntfile.js
+++ gruntfile.js
@@ -3,11 +3,43 @@ module.exports = function(grunt) {
   grunt.initConfig({
     jshint: {
       all: ['js/*.js']
-    }
+    },
+	sass: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'style.css': 'style.sass'
+        }
+      }
+    },
+    imagemin: {
+	    dynamic: {
+	        files: [{
+	            expand: true,
+	            cwd: 'images/',
+	            src: ['**/*.{png,jpg,gif}'],
+	            dest: 'images/build/'
+	        }]
+	    }
+	},
+	watch: {
+	    scripts: {
+	        files: ['sass/*.sass'],
+	        tasks: ['sass'],
+	        options: {
+	            spawn: false,
+	        },
+    	}
+  	}
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint', 'sass', 'imagemin', 'watch']);
 
 };
