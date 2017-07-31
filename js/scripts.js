7b04686f2707668732937d84eec54a257a2ec6de
pkomolkoe <p.komolko@gmail.com>
2017-08-01 00:29:13 +0200

    zadanie-8.5

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,24 @@
+
 // scripts.js
 
-var name = prompt('Enter your name');
-alert('Hello, ' + name);
-console.log('Hello, ' + name);
+
+var a = 3;
+var h = 6;
+var triangleArea = a*h/2;
+console.log('Triangle field with base a: ' + a + 
+	' and height h: ' + h + ' is equal to: ' + triangleArea + '.');
+
+
+// opcja 2
+
+var b = prompt('podaj liczbę');
+var c = prompt('podaj liczbę');
+
+var podstawa = parseInt(b);
+var wysokosc = parseInt(c);
+var poleTrojkata = podstawa*wysokosc/2;
+
+alert('pole trójkąta o boku: ' + podstawa + ' i wysokości ' + wysokosc + ' wynosi ' + poleTrojkata);
+
+
+
