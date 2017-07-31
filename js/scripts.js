634ff8a1bb2a76aa8e63567061724afc94e6a032
pkomolkoe <p.komolko@gmail.com>
2017-08-01 00:15:18 +0200

    zadanie8.4

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
 // scripts.js
 
 var name = prompt('Enter your name');
-alert('Hello, ' + name);
+//alert('Hello, ' + name);
 console.log('Hello, ' + name);
+
+// komentarz 1
+// komentarz 2
+
+/* 
+	komentarz 3 
+	bardzo długi
+	jest ten komentarz
+*/
+
