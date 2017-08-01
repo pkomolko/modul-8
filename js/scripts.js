baa818aad40dd146843c9af044d97cc64fa00c3a
pkomolkoe <p.komolko@gmail.com>
2017-08-01 14:15:30 +0200

    zadanie8.6

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,35 @@
-// scripts.js
 
-var name = prompt('Enter your name');
-alert('Hello, ' + name);
-console.log('Hello, ' + name);
+
+var a = parseInt(prompt('podaj liczbę'));
+var b = parseInt(prompt('podaj drugą liczbę'));
+
+var wynikRownania = (a * a) + (2 * a * b) - (b * b);
+
+alert (' wynik równania dla liczby ' + a + ' oraz ' + b + ' wynosi ' + wynikRownania + ' . ')
+
+/*if (wynikRownania >= 0) {
+    alert('wynik dodatni');
+  } else {
+    alert('wynik ujemny');
+  }
+
+if (wynikRownania = 0) {
+    alert('wynik jest zerem');
+  } else {
+    alert('wynik nie jest zerem');
+  }
+*/
+
+if (wynikRownania > 0) {
+
+    alert('wynik dodatni');
+
+} else if (wynikRownania < 0) {
+
+    alert('wynik dodatni');
+
+} else {
+
+    alert('wynik jest zerem');
+
+}
